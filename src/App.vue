169cca5c5f0c6aac287ccc2dfe7f0ff0bff3ccb5
Remon9773 <rschokker020@gmail.com>
2022-09-03 20:09:09 +0200

    stop deactivated, rectangle movements

--- src/App.vue
+++ src/App.vue
@@ -13,11 +13,19 @@ if (import.meta.hot?.on('vite:beforeUpdate', () => {
 onMounted(() => {
     const sketch = Sketch('sketch1', {w: 400, h: 400, bg: 'lightgray', center: true})
     const {stop, width, height, Vector, Vec} = sketch;
-
+    let x = 0;
+    let y = height-30;
+    let velX = 2;
+    let velY = 2;
     sketch.draw(({time, clear, fill, circle, rect, line, stroke, strokeWeight, text, textWeight, textMode}) => {
         clear();
+        if (x > width-100) velX = 0;
+        if (y < 0 ) velY = 0;
+        x += velX;
+        y -= velY;
+        rect(x, y, 100, 30);
 
     });
-    stop();
+    // stop();
 });
 </script>
