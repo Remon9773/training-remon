4757b285485af6d3e4fd55c60f5b334b8a66d0e9
Remon9773 <rschokker020@gmail.com>
2022-09-04 20:31:16 +0200

    change rectangle to circle

--- src/App.vue
+++ src/App.vue
@@ -13,17 +13,32 @@ if (import.meta.hot?.on('vite:beforeUpdate', () => {
 onMounted(() => {
     const sketch = Sketch('sketch1', {w: 400, h: 400, bg: 'lightgray', center: true})
     const {stop, width, height, Vector, Vec} = sketch;
-    let x = 0;
-    let y = height-30;
-    let velX = 2;
-    let velY = 2;
+    let x = 50*3;
+    let y = height/2;
+    let x2 = width/2;
+    let y2 = -20;
+    let velX = 1;
+    let velY = 1;
+    let velY2 = 0;
+    let rood1 = 255;
+    let groen1 = 0;
+    let blauw1 = 0;
     sketch.draw(({time, clear, fill, circle, rect, line, stroke, strokeWeight, text, textWeight, textMode}) => {
         clear();
-        if (x > width-100) velX = 0;
-        if (y < 0 ) velY = 0;
-        x += velX;
-        y -= velY;
+
+        if (y > height){
+            velY = 0;
+            velY2 = 1;
+        }
+
+
+        y += velY;
+        y2 += velY2;
+        fill(rood1, groen1, blauw1);
         rect(x, y, 100, 30);
+
+        fill(0, 255, 0);
+        circle(x2, y2, 20);
 
     });
     // stop();
