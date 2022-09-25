88aaad0cd2acdc8ac1f96c210d30874d1b5377f6
Remon9773 <rschokker020@gmail.com>
2022-09-25 16:07:29 +0200

    rects with objects 1

--- src/App.vue
+++ src/App.vue
@@ -13,46 +13,38 @@ if (import.meta.hot?.on('vite:beforeUpdate', () => {
 onMounted(() => {
     const sketch = Sketch('sketch1', {w: 400, h: 400, bg: 'black', center: true})
     const {stop, width, height, Vector, Vec} = sketch;
-    // rect 1
-    let x1 = 0;
-    let y1 = 0;
-    // rect 2
-    let x2 = width-30;
-    let y2 = 0;
-    // rect 3
-    let x3 = 0;
-    let y3 = height-30;
-    // rect 4
-    let x4 = width-30;
-    let y4 = height-30;
+    
+    const rectangle = (x, y, w, h, color) => {
+        x = x;
+        y = y;
+        w = w;
+        h = h;
+        const c = {
+            r: color.r,
+            g: color.g,
+            b: color.b,
+        }
+        return {
+            x, y, w, h, color: c,
+        };
+    }
+    const rect1 = rectangle(0, 0, 30, 30, {r: 255, g: 0, b: 0});
+    const rect2 = rectangle(width-30, 0, 30, 30, {r: 0, g: 255, b: 0});
+    const rect3 = rectangle(0, height-30, 30, 30, {r: 0, g: 0, b: 255});
+    const rect4 = rectangle(width-30, height-30, 30, 30, {r: 255, g: 0, b: 255});
+
+
     sketch.draw(({time, clear, fill, circle, rect, line, stroke, strokeWeight, text, textWeight, textMode}) => {
         clear();
-        // rect 1
-        x1++;
-        y1++;
-        fill(255, 0, 0);
-        if (x1 < width/2 - 15) rect(x1, y1, 30, 30);
-        else circle(x1 + 15, y1 + 15, 15);
-        
-        // rect 2
-        x2--;
-        y2++;
-        fill(0, 255, 0);
-        if (x2 > width/2 - 15) rect(x2, y2, 30, 30);
-        else circle(x2 + 15, y2 + 15, 15);
-        
-        
-        // rect 3
-        x3++;
-        y3--;
-        fill(0, 0, 255);
-        rect(x3, y3, 30, 30);
-        // rect 4
-        x4--;
-        y4--;
-        fill(255, 255, 0);
-        rect(x4, y4, 30, 30);
 
+        fill(rect1.color.r, rect1.color.g, rect1.color.b);
+        rect(rect1.x, rect1.y, rect1.w, rect1.h);
+        fill(rect2.color.r, rect2.color.g, rect2.color.b);
+        rect(rect2.x, rect2.y, rect2.w, rect2.h);
+        fill(rect3.color.r, rect3.color.g, rect3.color.b);
+        rect(rect3.x, rect3.y, rect3.w, rect3.h);
+        fill(rect4.color.r, rect4.color.g, rect4.color.b);
+        rect(rect4.x, rect4.y, rect4.w, rect4.h);
     });
     // stop();
 });
